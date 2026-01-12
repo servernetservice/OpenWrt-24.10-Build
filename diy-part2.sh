1bd4fd5cf100354a14dce39b204c26421ce566a5
servernetservice <servernet.service@gmail.com>
2026-01-12 16:35:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 
-# 彻底清理冲突源
+# 清理冲突包
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -13,8 +12,9 @@ rm -rf feeds/luci/applications/luci-app-adguardhome
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 强制注入配置，防止被 defconfig 篡改
+# 强制锁定核心配置，防止被 defconfig 篡改
 echo "CONFIG_TARGET_x86=y" >> .config
 echo "CONFIG_TARGET_x86_64=y" >> .config
 echo "CONFIG_TARGET_x86_64_DEVICE_generic=y" >> .config
 echo "CONFIG_PACKAGE_dnsmasq-full=y" >> .config
+echo "CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y" >> .config
