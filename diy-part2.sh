186ad2712ed51f8b2991469a8e563824000aaa45
servernetservice <servernet.service@gmail.com>
2026-01-12 16:17:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,12 +3,17 @@
 # 修改默认主题为 bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 
-# 修复某些插件可能导致的依赖冲突
 # 移除可能冲突的重复包
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-adguardhome
 
-# 重新同步 feeds 以确保使用我们添加的第三方源
+# 修复 AdGuardHome 路径问题（针对某些源层级过深的问题）
+if [ -d "feeds/adguardhome/luci-app-adguardhome" ]; then
+    mv feeds/adguardhome/luci-app-adguardhome package/
+fi
+
+# 重新同步 feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
