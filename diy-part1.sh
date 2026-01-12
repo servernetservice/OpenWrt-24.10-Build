1c48277dfdf14f8bd0fbc204aae8d51902db1fb1
servernetservice <servernet.service@gmail.com>
2026-01-12 16:17:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
+# 添加 Passwall 及其依赖源
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+
+# 添加 SmartDNS
 echo 'src-git smartdns https://github.com/pymumu/luci-app-smartdns.git;master' >>feeds.conf.default
-echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git;master' >>feeds.conf.default
+
+# 使用更兼容的 AdGuardHome 源
+echo 'src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome.git;master' >>feeds.conf.default
